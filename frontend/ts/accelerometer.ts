81d8d53b4af2eadccc68aa254d5bc3f981851e56
mainnika <nikita@tokarch.uk>
2016-04-23 02:36:10 +0300

    calibrate y axis on first received motion event

--- frontend/ts/accelerometer.ts
+++ frontend/ts/accelerometer.ts
@@ -6,15 +6,28 @@ export interface IMotionEvent {
 }
 
 export class Accelerometer extends EventEmitter {
+    
+    private _inversed: boolean;
+    private _calibrated: boolean;
 
 	constructor() {
 		super();
+        
+        this._calibrated = false;
+        this._inversed = false;
+        
 		window.addEventListener('devicemotion', this.onSensor.bind(this), false);
 	}
 
 	private onSensor(event: DeviceMotionEvent) {
+        
+        if (!this._calibrated) {
+            this._inversed = event.accelerationIncludingGravity.y < 0;
+            this._calibrated = true; 
+        }
+        
 		let x = ~~(event.accelerationIncludingGravity.x * 10);
-		let y = ~~(event.accelerationIncludingGravity.y * 10);
+		let y = ~~(event.accelerationIncludingGravity.y * (this._inversed ? -10 : 10));
 
 		let motion: IMotionEvent = {
 			x: x,
